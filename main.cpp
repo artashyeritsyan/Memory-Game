90882e898d542fc5a6f69bc5087ca9c11d26b391
artashyeritsyan <yeritsyanartash@gmail.com>
2024-12-09 22:10:48 +0400

    test push

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
+#include "controller.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    // MainWindow w;
+    // w.show();
+
+    Controller controller;
+    // controller.start();
+
     return a.exec();
 }
